03258ce8ac3635eed9b9ff9f41aa3ee310fe22d8
suroor <surusuroor@gmail.com>
2015-11-20 21:45:35 +0530

    Count neighbours : test passed

--- game_of_life.py
+++ game_of_life.py
@@ -4,9 +4,21 @@ def isdead(x, y, grid) :
     else :
         return False
 
-# def count_live_neighbours(x, y, grid) :
-#     count = 0
-#     if 
+def count_live_neighbours(x, y, grid) :
+    count = 0
+    for dx in [-1, 0, 1] :
+        for dy in [-1, 0, 1] :
+            if dx == 0 and dy == 0 :
+                continue
+            elif x + dx < 0 or y + dy < 0 :
+                continue
+            else :
+                try:
+                    count += grid[x + dx][y + dy]
+                except IndexError :
+                    continue
+    return count
+
 
 def set_alive(x, y, grid) :
     grid[x][y] = 1
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,6 +1,8 @@
 import game_of_life
 
-test_data = [[0,0,1,1], [0,0,1,0], [1,0,0,0]]
+test_data = [[0,0,1],
+             [0,0,1],
+             [1,1,0]]
 
 def test_isdead_true() :
     dead = game_of_life.isdead(0,0,test_data)
@@ -10,9 +12,26 @@ def test_isdead_false() :
     dead = game_of_life.isdead(0, 2, test_data)
     assert dead == False
 
-# def test_count_live_neighbours() :
-#     count = game_of_life.count_live_neighbours(0, 2, test_data)
-#     assert count == 2
+def test_count_live_neighbours() :
+    count = game_of_life.count_live_neighbours(0, 0, test_data)
+    assert count == 0
+    count = game_of_life.count_live_neighbours(0, 1, test_data)
+    assert count == 2
+    count = game_of_life.count_live_neighbours(0, 2, test_data)
+    assert count == 1
+    count = game_of_life.count_live_neighbours(1, 0, test_data)
+    assert count == 2
+    count = game_of_life.count_live_neighbours(1, 1, test_data)
+    assert count == 4
+    count = game_of_life.count_live_neighbours(1, 2, test_data)
+    assert count == 2
+    count = game_of_life.count_live_neighbours(2, 0, test_data)
+    assert count == 1
+    count = game_of_life.count_live_neighbours(2, 1, test_data)
+    assert count == 2
+    count = game_of_life.count_live_neighbours(2, 2, test_data)
+    assert count == 2
+    
 
 def test_set_alive() :
     game_of_life.set_alive(0, 0, test_data)
